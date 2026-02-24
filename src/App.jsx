75656667f4f4f3dbc571e7948f998cd7ddf48050
Maria Zak <mzakart@gmail.com>
2026-02-24 17:21:15 -0500

    Bugs fix page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
-import Header from './components/Header' 
-import SitePage from './pages/SitePage' 
-import './App.scss' 
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import Header from "./components/Header"
 
-function App() { 
-  return ( 
-  <> <div className="topbar"> 
-  <Header /> 
-  </div> 
-  <main className="page"> 
-    
-    <SitePage/> 
-    </main> 
-    </> ) 
-    } export default App
+// import Hero from "./pages/Hero"
+// import About from "./pages/About"
+// import BugPortfolio from "./pages/BugPortfolio"
+// import Projects from "./pages/Projects"
+// import Contact from "./pages/Contact"
+
+// import "./App.scss"
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Header />
+
+//       <main className="page">
+//         <Routes>
+//           <Route path="/" element={<Hero />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/portfolio" element={<BugPortfolio />} />
+//           <Route path="/projects" element={<Projects />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Routes>
+//       </main>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Hero from "./pages/Hero"
+import About from "./pages/About"
+import BugPortfolio from "./pages/BugPortfolio"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
+import "./App.scss"
+
+  {/* add div to header and all App */}
+{/* centre the whole site 
+make look more modern with transparence and flow  */}
+
+export default function App() {
+  return (
+  
+ <div className="app">
+      <Header />
+      <main className="page">
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<BugPortfolio />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      </div>
+   
+  )
+}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,43 +1,30 @@
-// import './Nav.scss'
+import { Link } from "react-router-dom"
+import "./Nav.scss"
 
-// export default function Nav() {
-//   return (
-//     <nav className="nav">
-//       <a className="nav__btn nav__btn--blue" href="#about">ABOUT ME</a>
-//       <a className="nav__btn nav__btn--yellow" href="#portfolio">BUG PORTFOLIO</a>
-//       <a className="nav__btn nav__btn--green" href="#projects">PROJECTS</a>
-//       <a className="nav__btn nav__btn--red" href="#contact">CONTACT</a>
-//     </nav>
-//   )
-// }
-
-import './Nav.scss'
-
-import btnAbout from '../assets/buttons/btn-about.png'
-import btnPortfolio from '../assets/buttons/btn-portfolio.png'
-import btnContact from '../assets/buttons/btn-contact.png'
-import btnProjects from '../assets/buttons/btn-projects.png'
+import btnAbout from "../assets/buttons/btn-about.png"
+import btnPortfolio from "../assets/buttons/btn-portfolio.png"
+import btnContact from "../assets/buttons/btn-contact.png"
+import btnProjects from "../assets/buttons/btn-projects.png"
 
 export default function Nav() {
   return (
     <nav className="nav">
 
-      <a href="#about">
+      <Link to="/about">
         <img src={btnAbout} alt="About Me" />
-      </a>
+      </Link>
 
-      <a href="#portfolio">
+      <Link to="/portfolio">
         <img src={btnPortfolio} alt="Bug Portfolio" />
-      </a>
+      </Link>
 
-      <a href="#projects">
+      <Link to="/projects">
         <img src={btnProjects} alt="Projects" />
-      </a>
-      
-      <a href="#contact">
-        <img src={btnContact} alt="Contact" />
-      </a>
+      </Link>
 
+      <Link to="/contact">
+        <img src={btnContact} alt="Contact" />
+      </Link>
 
     </nav>
   )
